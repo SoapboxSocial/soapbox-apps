30483f7e6c84cb75461c7e143921d260770573b6
Jeff Reiner <theundeadghost@gmail.com>
2021-02-25 17:30:10 +0100

    Adding reroll

--- views/random/index.tsx
+++ views/random/index.tsx
@@ -1,7 +1,9 @@
 import { useMap } from "@roomservice/react";
 import { getMembers, User } from "@soapboxsocial/minis.js";
-import { useEffect } from "react";
-import { useSoapboxRoomId } from "../../hooks";
+import { useCallback, useEffect } from "react";
+import { RefreshCw } from "react-feather";
+import { CircleIconButton } from "../../components/inputs/button";
+import { useParams, useSoapboxRoomId } from "../../hooks";
 import getRandom from "../../lib/getRandom";
 import LoadingView from "../loading";
 
@@ -11,48 +13,56 @@ type RandomMap = {
 };
 
 export default function RandomView() {
+  const { isAppOpener } = useParams();
+
   const soapboxRoomId = useSoapboxRoomId();
   const roomServiceRoomName = `soapbox-mini-random-${soapboxRoomId}`;
 
   const [random, map] = useMap<RandomMap>(roomServiceRoomName, "random");
 
-  useEffect(() => {
-    async function selectRandomUser() {
-      if (map?.set) {
-        const members = await getMembers();
+  const selectRandomUser = useCallback(async () => {
+    if (map?.set) {
+      const members = await getMembers();
 
-        map?.set("chosen", members[getRandom(members.length)]);
-      }
+      map?.set("chosen", members[getRandom(members.length)]);
     }
-
-    selectRandomUser();
   }, [map]);
+
+  useEffect(() => {
+    selectRandomUser();
+  }, [map, selectRandomUser]);
 
   if (random?.chosen)
     return (
-      <main className="flex flex-col min-h-screen">
-        <div className="pt-4 px-4">
-          <div className="relative">
-            <h1 className="text-title2 font-bold text-center">Random Person</h1>
-          </div>
+      <main className="relative grid place-items-center min-h-screen">
+        <div className="absolute top-4">
+          <h1 className="text-title2 font-bold text-center">Random Person</h1>
         </div>
 
-        <div className="flex-1 p-4 flex justify-center items-center">
-          <div className="space-y-4">
-            <img
-              alt={random.chosen.display_name}
-              className="rounded-full ring-4 ring-soapbox"
-              draggable={false}
-              height={96}
-              loading="eager"
-              src={`https://cdn.soapbox.social/images/${random.chosen.image}`}
-              width={96}
+        {isAppOpener && (
+          <div className="absolute top-4 right-4">
+            <CircleIconButton
+              type="button"
+              icon={<RefreshCw size={20} />}
+              onClick={selectRandomUser}
             />
-
-            <p className="text-title1 font-bold text-center">
-              {random.chosen.display_name}
-            </p>
           </div>
+        )}
+
+        <div className="space-y-4">
+          <img
+            alt={random.chosen.display_name}
+            className="rounded-full ring-4 ring-soapbox"
+            draggable={false}
+            height={96}
+            loading="eager"
+            src={`https://cdn.soapbox.social/images/${random.chosen.image}`}
+            width={96}
+          />
+
+          <p className="text-title1 font-bold text-center">
+            {random.chosen.display_name}
+          </p>
         </div>
       </main>
     );
