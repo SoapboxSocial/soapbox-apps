ac549a3b2e620431f1b951d444595fdcc18356d8
Jeff Reiner <theundeadghost@gmail.com>
2021-03-29 13:53:29 +0200

    Refactor Random app

--- pages/random/index.tsx
+++ pages/random/index.tsx
@@ -1,26 +1,11 @@
-import { RoomServiceProvider } from "@roomservice/react";
-import { useSession, useSoapboxRoomId } from "../../hooks";
-import { AuthFunction } from "../../lib/roomservice";
-import LoadingView from "../../views/loading";
+import { PusherProvider } from "@harelpls/use-pusher";
+import { config } from "../../lib/pusher";
 import RandomView from "../../views/random";
 
 export default function Random() {
-  const soapboxRoomId = useSoapboxRoomId();
-  const user = useSession();
-
-  const isOnline = user !== null && soapboxRoomId !== null;
-
   return (
-    <RoomServiceProvider
-      online={isOnline}
-      clientParameters={{
-        auth: AuthFunction,
-        ctx: {
-          userID: String(user?.id),
-        },
-      }}
-    >
-      {isOnline ? <RandomView /> : <LoadingView />}
-    </RoomServiceProvider>
+    <PusherProvider {...config}>
+      <RandomView />
+    </PusherProvider>
   );
 }
--- views/random/index.tsx
+++ views/random/index.tsx
@@ -1,50 +1,81 @@
-import { useMap } from "@roomservice/react";
+import { useChannel, useEvent, usePusher } from "@harelpls/use-pusher";
 import { getMembers, onClose, User } from "@soapboxsocial/minis.js";
 import { AnimatePresence, motion } from "framer-motion";
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { RefreshCw } from "react-feather";
+import useSWR from "swr";
 import { CircleIconButton } from "../../components/inputs/button";
 import Spinner from "../../components/spinner";
 import { useParams, useSoapboxRoomId } from "../../hooks";
-import getRandom from "../../lib/getRandom";
 
-type RandomMap = {
-  chosen: User;
+const SERVER_BASE = process.env.NEXT_PUBLIC_APPS_SERVER_BASE_URL as string;
+
+const getRandomMember = async (_: string, roomID: string) => {
+  try {
+    const members = await getMembers();
+
+    await fetch(`${SERVER_BASE}/random/${roomID}/choose-member`, {
+      method: "POST",
+      body: JSON.stringify({ members: members }),
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error) {
+    console.error(error);
+  }
 };
+
+function useRandomMember(roomID: string, isAppOpener: boolean) {
+  const shouldFetch =
+    typeof window !== "undefined" &&
+    typeof roomID === "string" &&
+    isAppOpener === true;
+
+  return useSWR(
+    shouldFetch ? ["RandomMember", roomID] : null,
+    getRandomMember,
+    {
+      refreshWhenHidden: false,
+      refreshWhenOffline: false,
+      revalidateOnFocus: false,
+      revalidateOnReconnect: false,
+    }
+  );
+}
 
 export default function RandomView() {
   const { isAppOpener } = useParams();
 
   const soapboxRoomId = useSoapboxRoomId();
-  const roomServiceRoomName = `soapbox-mini-random-${soapboxRoomId}`;
+  const channelName = `mini-random-${soapboxRoomId}`;
 
-  const [random, map] = useMap<RandomMap>(roomServiceRoomName, "random");
+  const { client } = usePusher();
+  const channel = useChannel(channelName);
 
-  const [isLoading, isLoadingSet] = useState(false);
+  const { revalidate, isValidating } = useRandomMember(
+    soapboxRoomId,
+    isAppOpener
+  );
 
-  const selectRandomUser = useCallback(async () => {
-    map?.set("chosen", null);
+  /**
+   * 'member' Event Handling
+   */
+  const [member, memberSet] = useState<User>();
 
-    isLoadingSet(true);
+  useEvent(channel, "member", (data: { member: User }) => {
+    console.log("Received 'member' event with payload", data);
 
-    const members = await getMembers();
-
-    map?.set("chosen", members[getRandom(members.length)]);
-
-    isLoadingSet(false);
-  }, [map]);
+    memberSet(data.member);
+  });
 
   useEffect(() => {
-    if (!random?.chosen) {
-      selectRandomUser();
+    if (soapboxRoomId) {
+      onClose(() => {
+        memberSet(null);
+
+        client?.disconnect();
+      });
     }
-  }, [map, selectRandomUser]);
-
-  useEffect(() => {
-    onClose(() => {
-      map?.delete("chosen");
-    });
-  }, []);
+  }, [soapboxRoomId]);
 
   return (
     <main className="relative grid place-items-center min-h-screen">
@@ -55,34 +86,36 @@ export default function RandomView() {
       {isAppOpener && (
         <div className="absolute top-4 right-4">
           <CircleIconButton
-            loading={isLoading}
+            loading={isValidating}
             type="button"
             icon={<RefreshCw size={20} />}
-            onClick={selectRandomUser}
+            onClick={revalidate}
           />
         </div>
       )}
 
       <AnimatePresence>
-        {random?.chosen ? (
+        {member ? (
           <motion.div
             initial={{ opacity: 0, scale: 0.5 }}
             animate={{ opacity: 1, scale: 1 }}
             exit={{ opacity: 0, scale: 0.5 }}
             className="flex flex-col items-center space-y-4"
           >
+            <p className="text-6xl">👑</p>
+
             <img
-              alt={random.chosen.display_name}
+              alt={member.display_name}
               className="w-24 h-24 rounded-full ring-4 ring-soapbox bg-soapbox"
               draggable={false}
               height={96}
               loading="eager"
-              src={`https://cdn.soapbox.social/images/${random.chosen.image}`}
+              src={`https://cdn.soapbox.social/images/${member.image}`}
               width={96}
             />
 
             <p className="text-title2 font-bold text-center">
-              {random.chosen.display_name}
+              {member.display_name}
             </p>
           </motion.div>
         ) : (
