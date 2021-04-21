37e0945d2aa99085eb5e9a668503c98a85d634d5
Jeff Reiner <theundeadghost@gmail.com>
2021-04-21 15:25:21 +0200

    Update birds deploy

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
     return [
       {
         source: "/birds",
-        destination: "https://birds-k74q3.ondigitalocean.app/birds",
+        destination: "https://birds-ufb5n.ondigitalocean.app/birds",
         permanent: false,
       },
     ];
