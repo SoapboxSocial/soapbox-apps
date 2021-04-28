6908d74aba40069d41d1867d921f08c86410b8c6
Jeff Reiner <theundeadghost@gmail.com>
2021-04-28 12:17:38 +0200

    Adding werewolf page stubbed, removing placeholder pages

---
+++ pages/werewolf/index.tsx
@@ -0,0 +1,5 @@
+import WerewolfView from "../../views/werewolf";
+
+export default function Werewolf() {
+  return <WerewolfView />;
+}
---
+++ views/werewolf/index.tsx
@@ -0,0 +1,10 @@
+export default function WerewolfView() {
+  return (
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem explicabo
+      provident ratione nisi necessitatibus nostrum ex, labore expedita
+      obcaecati, rerum quasi ipsa, libero doloremque accusantium. Totam officiis
+      nobis vel rerum?
+    </p>
+  );
+}
